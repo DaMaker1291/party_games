b25e9c35b2ff518d7e763c5520c9d4ca4108cc8e
DaMaker1291 <storage.me29@gmail.com>
2026-06-14 00:17:46 +0100

    Fix deprecated meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <meta name="theme-color" content="#0a0a0f" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <title>Party Games!</title>
         <link rel="manifest" href="/manifest.json" />
